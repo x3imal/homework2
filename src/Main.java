eb2ed025fb29394ec2ef2901c1d31fe8a88193ce
x3imal <aasurov@gmail.com>
2023-02-06 15:17:54 +0300

    commit task 2 end

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,29 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-        ... // Пишем код для задачи 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);// Пишем код для задачи 1
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-        ... // Пишем код для задачи 2
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // Пишем код для задачи 2
     }
+    public static void task2 () {
+        System.out.println("Задача 3");
+
 }
